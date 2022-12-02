6860f30ad3b3ce64fb3e225f9224cb50f5497e46
Ben Wright <ben.wright@ammcoats.com>
2022-12-02 15:37:58 -0600

    add 2021 day 1

---
+++ 2021_day1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace _2021_day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2021 Day 1!");
+            int L = 0, S = 0;
+            int sumGreater = 0;
+
+            using (var stream = File.OpenRead("input.txt"))
+            using (var sr = new StreamReader(stream))
+            {
+                string line = null;
+
+                var window = new int[3];
+                int sum = 0, sumPrior = 0;
+                for (int prior = -1, w = 0, cnt = 0; 
+                    null != (line = sr.ReadLine())
+                        && int.TryParse(line, out int meas); 
+                    prior = meas, window[w] = meas, w = (w + 1) % window.Length, sumPrior = sum, cnt++)
+                {
+                    sum += meas - window[w];
+
+                    if (prior < 0) continue;
+
+                    if (meas > prior)
+                        L++;
+                    else if (meas < prior)
+                        S++;
+
+                    if (cnt >= window.Length && sum > sumPrior)
+                        sumGreater++;
+                }
+            }
+
+            Console.WriteLine($"Larger: {L}");
+            Console.WriteLine($"sumGreater: {sumGreater}");
+        }
+    }
+}
