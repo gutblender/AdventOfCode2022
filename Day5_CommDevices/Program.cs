05f6f881a6e57a91eb63e50f5e0254e4a7565366
Ben Wright <ben.wright@ammcoats.com>
2022-12-05 23:22:57 -0600

    add 2022 day 5

---
+++ Day5_CommDevices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day5_CommDevices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
